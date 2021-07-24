b0661c50f50394053e1a584ae5e7c9818f3248bb
Mehdi Hassan Akash <mhakash211998@gmail.com>
2021-07-24 09:19:43 +0600

    add file-picker

---
+++ components/FilePicker.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+import Upload from "./Icons/Upload";
+
+const FilePicker = ({ setFile, file }) => {
+  const handleDragEvent = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const files = [...e.dataTransfer.files];
+    console.log(files);
+    setFile(files);
+  };
+
+  return (
+    <div
+      className=""
+      onDragOver={(e) => handleDragEvent(e)}
+      onDragEnter={(e) => handleDragEvent(e)}
+      onDragLeave={(e) => handleDragEvent(e)}
+      onDrop={(e) => handleDrop(e)}
+    >
+      <label>
+        <input
+          type="file"
+          className="hidden"
+          multiple={false}
+          onChange={(e) => setFile([...e.target.files])}
+        />
+        <div className="w-96 h-48 rounded-md max-w-full flex flex-col justify-center items-center m-auto border-gray-800 border  cursor-pointer">
+          <Upload className="w-16 h-16" fill="rgb(55, 65, 81)" />
+          <div>click / drag</div>
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default FilePicker;
---
+++ pages/filePicker.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import FilePicker from "../components/FilePicker";
+
+const FilePickerPage = () => {
+  const [file, setFile] = useState([]);
+
+  return (
+    <div className="my-8 text-center">
+      <div className="mb-4">File Picker</div>
+      <FilePicker file={file} setFile={setFile} />
+      {file && (
+        <div className="mt-4">
+          {file.map((e) => (
+            <div key={e.name}>{e.name}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FilePickerPage;
